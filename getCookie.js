93c72876f740dd7e3564bdd521a7f195c64ecf59
aiboboxx <aiboboxx@gmail.com>
2021-03-23 13:30:44 +0800

    new commit

--- getCookie.js
+++ getCookie.js
@@ -2,25 +2,25 @@
 此文件为Node.js专用。其他用户请忽略
  */
 //此处填写京东账号cookie。
-//注：github action用户cookie填写到Settings-Secrets里面，新增FREEOK_COOKIS，多个账号的cookie使用`&`隔开或者换行
+//注：github action用户cookie填写到Settings-Secrets里面，新增FREEOK_COOKIES，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
   '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
-if (process.env.FREEOK_COOKIS) {
-  if (process.env.FREEOK_COOKIS.indexOf('&') > -1) {
+if (process.env.FREEOK_COOKIES) {
+  if (process.env.FREEOK_COOKIES.indexOf('&') > -1) {
     console.log(`您的cookie选择的是用&隔开\n`)
-    CookieJDs = process.env.FREEOK_COOKIS.split('&');
-  } else if (process.env.FREEOK_COOKIS.indexOf('\n') > -1) {
+    CookieJDs = process.env.FREEOK_COOKIES.split('&');
+  } else if (process.env.FREEOK_COOKIES.indexOf('\n') > -1) {
     console.log(`您的cookie选择的是用换行隔开\n`)
-    CookieJDs = process.env.FREEOK_COOKIS.split('\n');
-  } else if (process.env.FREEOK_COOKIS.indexOf('\\n') > -1) {
+    CookieJDs = process.env.FREEOK_COOKIES.split('\n');
+  } else if (process.env.FREEOK_COOKIES.indexOf('\\n') > -1) {
     //环境变量兼容腾讯云和docker下\n会被转义成\\n
     console.log(`您的cookie选择的是用换行隔开\\n`)
-    CookieJDs = process.env.FREEOK_COOKIS.split('\\n');
+    CookieJDs = process.env.FREEOK_COOKIES.split('\\n');
   } else {
-    CookieJDs = [process.env.FREEOK_COOKIS];
+    CookieJDs = [process.env.FREEOK_COOKIES];
   }
   CookieJDs = [...new Set(CookieJDs)]
   console.log(`\n====================共有${CookieJDs.length}个账号Cookie=========\n`);
