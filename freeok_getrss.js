af1f1b5c3318ea6b92af0dda02ec9e9c0ef4643f
aiboboxx <aiboboxx@gmail.com>
2021-04-04 22:17:18 +0800

    add features 2021/4/4

--- freeok_getrss.js
+++ freeok_getrss.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function  freeokSign (row,page) {
+async function  freeokBuy (row,page) {
     await page.goto('https://v2.freeok.xyz/auth/login');
   //await page.waitForSelector("#email");
     await page.type('#email', row.usr, {delay: 20});
@@ -16,36 +16,16 @@ async function  freeokSign (row,page) {
     ])
     .then( () =>  console.log ('登录成功'))
     .catch( (err) => console.log ("登录失败: "+ err));
-    try {
-/*       await page.waitForSelector('#checkin', {
-        timeout: 3000,
-      }) */
-      let checkin = await page.$('#checkin');
-      if (checkin) {
-       await Promise.all([
-         checkin.click(),
-         page.waitForNavigation({timeout: 5000})  
-       ])
-       .then( () =>  console.log('签到成功'))
-       .catch( (err) => console.log('签到超时'));
-       } else {
-        console.log('今日已签到');
-       }
-    }
-    catch(err) {
-      console.log('签到出错:' + err);
-    }
-    await page.waitFor(1000);
-    //获取余额
-    let inner_html = await page.evaluate( () => document.querySelector( 'body > main > div.container > section > div.ui-card-wrap > div:nth-child(2) > div > div.user-info-main > div.nodemain > div.nodemiddle.node-flex > div' ).innerHTML.trim());
-    inner_html = inner_html.split(' ')[0];
-    console.log( "余额: " + inner_html);
-    row.balance = Number(inner_html);
+    //await page.waitFor(1500);
+    //await page.goto('https://v2.freeok.xyz/user')
+    inner_html = await page.evaluate( () => document.querySelector( '#all_v2ray_windows > div.float-clear > input' ).value.trim());
+    console.log( "rss: " + inner_html);
+    row.rss = inner_html;
     //等级过期时间
-    inner_html = await page.evaluate( () => document.evaluate('/html/body/main/div[2]/section/div[1]/div[6]/div[1]/div/div/dl/dd[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerHTML );
-    inner_html = inner_html.split(';')[1];
-    console.log( "等级过期时间: " +  inner_html);
-    row.level_end_time = inner_html;
+    // inner_html = await page.evaluate( () => document.evaluate('/html/body/main/div[2]/section/div[1]/div[6]/div[1]/div/div/dl/dd[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.innerHTML );
+    // inner_html = inner_html.split(';')[1];
+    // console.log( "等级过期时间: " +  inner_html);
+    // row.level_end_time = inner_html;
     await page.goto('https://v2.freeok.xyz/user/logout');
     return row;
 }  
@@ -65,14 +45,15 @@ async function  main () {
         //console.info(`➞ ${dialog.message()}`);
         await dialog.dismiss();
     });
-    console.log(`*****************开始freeok签到 ${Date()}*******************\n`);  
-    var sql = "SELECT * FROM freeok"
+    console.log(`*****************开始freeok购买套餐 ${Date()}*******************\n`);  
+    var sql = "SELECT * FROM freeok WHERE rss is Null;"
     var r = await sqlite.all(sql, []);
+    console.log(`共有${r.length}个账户要购买套餐`);
     for (let row of r) {
       console.log("user:", row.id, row.usr);
-      if (row.usr&&row.pwd) await freeokSign(row,page).then(row => {
+      if (row.usr&&row.pwd) await freeokBuy(row,page).then(row => {
         //console.log(row);
-        sqlite.run("UPDATE freeok SET balance = ?, level_end_time = ?, update_time = datetime('now')  WHERE id = ?", [row.balance,row.level_end_time,row.id])
+        sqlite.run("UPDATE freeok SET rss = ?, update_time = datetime('now')  WHERE id = ?", [row.rss,row.id])
         .then((reslut)=>console.log(reslut))
       });
      }
