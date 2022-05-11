de8acc09e9d140ce434def7a899683dc53473f07
aiboboxx <aiboboxx@gmail.com>
2022-05-11 21:18:56 +0800

    fix bugs  20220511 211856

--- freeok_sign.js
+++ freeok_sign.js
@@ -88,7 +88,7 @@ async function freeokSign(row, page) {
       reset.rss = true;
       console.log("4天重置")
     }
-  if ((Date.now() - Math.max(...unixtimes)) / (60 * 60 * 1000) > (unixtimes[1] < unixtimes[2] ? 4 : 23) && row.level === 1 && row.count !== 0) {
+  if ((Date.now() - Math.max(...unixtimes)) / (60 * 60 * 1000) > (unixtimes[1] < unixtimes[2] ? 3 : 23) && row.level === 1 && row.count !== 0) {
       reset.pwd = true;
       reset.rss = true;
       //console.log('清空fetcher',new Date(row.regtime).format('yyyy-MM-dd hh:mm:ss'),new Date(row.last_used_time).format('yyyy-MM-dd hh:mm:ss'),new Date(row.fetch_time).format('yyyy-MM-dd hh:mm:ss'));
@@ -178,7 +178,7 @@ async function main() {
              FROM freeok 
              where level > 0 and (sign_time < date_sub(now(), interval 3 hour) or sign_time is null)
              order by sign_time asc 
-             limit 30;`
+             limit 20;`
   //sql = "SELECT * FROM freeok where err=1 order by sign_time asc;"
   //sql = "SELECT * FROM freeok where id=595"
   let r = await pool.query(sql, []);
