a197d0ca10a87b1413b40fd4d85457d8a1a3c459
aiboboxx <aiboboxx@gmail.com>
2022-06-01 11:05:44 +0800

    fix bugs  20220601 110544

--- freeok_regAuto.js
+++ freeok_regAuto.js
@@ -177,8 +177,8 @@ async function main() {
   let sql = "SELECT count(*) AS Number FROM freeok where level = 1;"
   let r = await pool.query(sql);
   //console.log(JSON.stringify(r))
-  if ( r[0][0].Number > 180 ) {
-    console.log('已有180个level=1账户');
+  if ( r[0][0].Number > 160 ) {
+    console.log('已有160个level=1账户');
     return;
   }
   console.log('已有账户：',r[0][0].Number);
