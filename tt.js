ebfc07b2def940766e1822c9111cc35cd3bc0b24
aiboboxx <aiboboxx@gmail.com>
2022-06-06 09:48:00 +0800

    fix bugs  20220606 094800

--- tt.js
+++ tt.js
@@ -1,7 +1,14 @@
 const { tFormat, sleep, clearBrowser, getRndInteger, randomOne, randomString } = require('./common.js');
 const { sbFreeok, login, loginWithCookies, resetPwd, resetRss } = require('./utils.js');
+const dayjs = require('dayjs')
+let utc = require('dayjs/plugin/utc') // dependent on utc plugin
+let timezone = require('dayjs/plugin/timezone')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault("Asia/Hong_Kong")
 Date.prototype.format = tFormat;
 let d = new Date()
 console.log(d.toLocaleString())
 console.log(d.toString())
 console.log(d.format('yyyy-MM-dd hh:mm:ss'))
+console.log(dayjs.tz().format('YYYY-MM-DD HH:mm:ss'))
