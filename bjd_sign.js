5780f2df7c2195bee7576cf8ee3ac8536e084d51
aiboboxx <aiboboxx@gmail.com>
2022-06-24 11:06:27 +0800

    fix bugs  20220624 110627

--- bjd_sign.js
+++ bjd_sign.js
@@ -60,9 +60,9 @@ async function freeokSign(row, page) {
       await pool.query("UPDATE freeok SET level = 0  WHERE id = ?", [row.id])
       return Promise.reject(new Error('账户即将失效'));
   }
-  if ((dayjs.tz().unix() -  dayjs.tz(row.fetch_time).unix()) / (24 * 60 * 60) > 7 && row.level === 1 && row.count !== 0) {
+  if ((dayjs.tz().unix() -  dayjs.tz(row.fetch_time).unix()) / (24 * 60 * 60) > 5 && row.level === 1 && row.count !== 0) {
      await pool.query("UPDATE freeok SET count = 0  WHERE id = ?", [row.id])
-     console.log("7天重置")
+     console.log("5天重置")
  }
   let selecter, innerHtml;
     //剩余流量
