c0f03b7f867b777fa956ed7d7ab4d834f90b324f
aiboboxx <aiboboxx@gmail.com>
2023-06-20 12:18:30 +0800

    fix bugs  20230620 121830

--- autolink.js
+++ autolink.js
@@ -5,6 +5,6 @@ const { chromium } = require('playwright');
   // Create pages, interact with UI elements, assert values
   const page = await browser.newPage();
   await page.goto('https://www.google.com/');
-  await page.screenshot({ path: `example.png` });
+  await page.screenshot({ path: `node_modules/example.png` });
   await browser.close();
 })();
