031d93ed2f8775db0967cebba502f80e260d2472
aiboboxx <aiboboxx@gmail.com>
2023-06-20 15:17:35 +0800

    fix bugs  20230620 151735

--- autolink.js
+++ autolink.js
@@ -4,7 +4,7 @@ const { chromium } = require('playwright');
   const browser = await chromium.launch({ headless: false, slowMo: 50 });
   // Create pages, interact with UI elements, assert values
   const page = await browser.newPage();
-  await page.goto('https://drone.cool/pipeline/triggers/')
+  await page.goto('https://www.bzshare.com/')
   .catch(async (error)=>{console.log('error: ', error.message);});
   await page.screenshot({ path: `node_modules/example.png` });
   await browser.close();
