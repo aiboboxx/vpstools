1a596b4284a9ef91c4ee296459cac4d083636b5d
aiboboxx <aiboboxx@gmail.com>
2023-10-21 17:18:54 +0800

    fix bugs  20231021 171854

---
+++ cfdns.js
@@ -0,0 +1,27 @@
+const fs = require("fs")
+const dns = require('dns');
+const dnsPromises = dns.promises;
+const domains = fs.readFileSync('domainlist.txt','utf8').split('\n');
+
+(async function() {   
+    for (const domain of domains){
+        console.log(domain); 
+        await dnsPromises.resolve4(domain)
+        .then((result) => { 
+            console.log(result); 
+            fs.appendFileSync('./output/domainlist.txt', domain + '\n')
+            for (const r of result){            
+                fs.appendFileSync('./output/domain2ip.txt', r + '\n')
+                fs.appendFileSync('./output/domain2ip.txt.info', domain + ' ' + r +'\n')
+            }
+        })
+        .catch((error)=>{console.log('error: ', error.message);})
+    }
+    console.log('Done')
+})(); 
+
+
+
+
+
+
---
+++ getCfip.js
@@ -0,0 +1,70 @@
+const fs = require("fs")
+const { chromium } = require('playwright-extra')
+const stealth = require('puppeteer-extra-plugin-stealth')()
+chromium.use(stealth)
+const { removeRepeatArray, sleep, clearBrowser, getRndInteger, randomOne, randomString,  md5 } = require('./common.js');
+const mysql = require('mysql2/promise')
+const setup = JSON.parse(fs.readFileSync('./setup.json', 'utf8'))
+const pool = mysql.createPool({
+  host: setup.mysql.host,
+  user: setup.mysql.user,
+  password: setup.mysql.password,
+  port: setup.mysql.port,
+  database: setup.mysql.database,
+  waitForConnections: true, //连接超额是否等待
+  connectionLimit: 10, //一次创建的最大连接数
+  queueLimit: 0, //可以等待的连接的个数
+  timezone: '+08:00',//时区配置
+  charset: 'utf8' //字符集设置
+});
+let item
+let runId = process.env.runId
+let browser
+
+
+async function launchBrowser() {
+  browser = await chromium.launch({
+    //headless: runId ? true : false,
+    headless: false,
+    args: [
+      '--window-size=1920,1080',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-blink-features=AutomationControlled',
+      //runId ? '' : setup.proxy.changeip,
+    ],
+    defaultViewport: null,
+    ignoreHTTPSErrors: true,
+  })
+}
+async function main() {
+  await launchBrowser()
+  const context = await browser.newContext()
+  const page = await browser.newPage()
+  page.setDefaultTimeout(20000);
+  const ips = fs.readFileSync('ip.txt','utf8').split('\n')
+  //const ips = ["116.214.34.66"]
+  //console.log(ips)
+  //return
+  let domains = []
+  for (const ip of ips){
+    await page.goto(`https://ipchaxun.com/${ip}/`)
+    await sleep(500)
+    let urls = []
+    let links =   page.locator('div[id="J_domain"] a')
+    //console.log('links个数：',await links.count())
+    //console.log(JSON.stringify(links))
+    urls = await links.evaluateAll(
+      list => list.map(element => element.href.replace("https://ipchaxun.com/","").replace("/","")));
+    //console.log(urls.join('\n'))
+    domains.push(...urls)
+  }
+
+  fs.writeFileSync('domains.txt', removeRepeatArray(domains).join('\n'))
+  console.log('Done')
+  // await pool.end()
+  // await page.close()
+  // await context.close()
+  // await browser.close()
+}
+main()
