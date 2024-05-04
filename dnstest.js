52faea4c6cf944ddc23b8f82f063be4a5c98aeaa
aiboboxx <aiboboxx@gmail.com>
2024-05-04 10:07:42 +0800

    fix bugs  20240504 100742

--- dnstest.js
+++ dnstest.js
@@ -1,7 +1,7 @@
 const dns = require('dns');
 const dnsPromises = dns.promises;
 (async () => {
-    await dnsPromises.resolve('cf.cdn.cmliu.net', 'A')
+    await dnsPromises.resolve('www.c333.net', 'A')
     .then(async (result) => {
         console.log(result);
     })
