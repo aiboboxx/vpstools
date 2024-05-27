6e20d3971ff29d12fccab41fccf2b86147b7581f
aiboboxx <aiboboxx@gmail.com>
2024-05-27 08:03:16 +0800

    fix bugs  20240527 080316

--- setpro2.js
+++ setpro2.js
@@ -28,8 +28,8 @@ const zones = ['jp','hk','sg','vn','us','ust','gb','de','tr'];
     }
     let sql = `SELECT ip
         FROM ip_fd
-        WHERE good_count > 2 and off = 1
-        ORDER BY good_count_time desc
+        WHERE good_count > 3 and off = 1 and (good_count_time < date_sub(now(), interval 2 day)
+        ORDER BY good_count desc
         limit 8;`
 
     let r = await pool.query(sql)
