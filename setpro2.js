29ae0ea5868ab395e3412dca4e724e842631bd2d
aiboboxx <aiboboxx@gmail.com>
2024-05-27 08:10:51 +0800

    fix bugs  20240527 081051

--- setpro2.js
+++ setpro2.js
@@ -28,9 +28,9 @@ const zones = ['jp','hk','sg','vn','us','ust','gb','de','tr'];
     }
     let sql = `SELECT ip
         FROM ip_fd
-        WHERE good_count > 3 and off = 1 and (good_count_time < date_sub(now(), interval 2 day)
+        WHERE good_count > 3 and off = 1 and good_count_time < date_sub(now(), interval 2 day)
         ORDER BY good_count desc
-        limit 8;`
+        limit 12;`
 
     let r = await pool.query(sql)
     console.log(`共有${r[0].length}个 ip`);
